b92ebaf64d42b85c46bf0c82d73f9ab0ba961c62
Yasin Hussein <bulaale116@gmail.com>
2024-01-16 15:31:09 -0700

    spinner1 and spinner 2

--- Asynchronous/spinner/spinner1.js
+++ Asynchronous/spinner/spinner1.js
@@ -1,38 +1,26 @@
 /* eslint-disable linebreak-style */
 process.stdout.write('hello from spinner1.js... \rheyyy\n');
 setTimeout(() => {
-  process.stdout.write('\r|   ');
+  process.stdout.write('\r|  C ');
 }, 100);
 
 setTimeout(() => {
-  process.stdout.write('\r/   ');
+  process.stdout.write('\r/  O');
 }, 300);
 
 setTimeout(() => {
-  process.stdout.write('\r-   ');
+  process.stdout.write('\r-   d');
 }, 500);
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   ');
+  process.stdout.write('\r\\  I ');
 }, 700);
 setTimeout(() => {
-  // Need to escape the forwardslash since it's a special character.
-  process.stdout.write('\r/ \'\' ');
-}, 900);
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\  N ');
+}, 800);
 setTimeout(() => {
-  process.stdout.write('\r \\/  ');
-}, 1100);
-
-
-
-/* eslint-disable linebreak-style */
-process.stdout.write('hello from spinner1.js... \rheyyy\n');
-let word = 'CODING';
-let backSlashes = ['|','','-','\\','/','\\'];
-for (let i = 0; i < word.length; i++) {
-  const timeDelay = 100 * (i + 1);
-  setTimeout(() => {
-    process.stdout.write(`\r${backSlashes[i]}  ${word[i]} `);
-  }, timeDelay);
-}
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\  G');
+}, 900);
--- Asynchronous/spinner/spinner2.js
+++ Asynchronous/spinner/spinner2.js
@@ -1,10 +1,13 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable space-before-blocks */
+/* eslint-disable linebreak-style */
 process.stdout.write('hello from spinner1.js... \rheyyy\n');
 let word = 'CODING';
-let backSlashes = ['|','','-','\\','/','\\'];
-for (let i = 0; i < word.length; i++) {
+ let backSlashes = ['|','','-','\\','/','\\'];
+for (let i = 0; i < word.length; i++){
   const timeDelay = 100 * (i + 1);
-  setTimeout(() => {
-    process.stdout.write(`\r${backSlashes[i]}  ${word[i]} `);
-  }, timeDelay);
+setTimeout(() => {
+  process.stdout.write(`\r${backSlashes[i]}  ${word[i]} `);
+}, timeDelay);
 }

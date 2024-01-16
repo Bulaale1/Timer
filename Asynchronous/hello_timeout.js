0e763ae0abb6c374c4ef5818cfb43cbf2eea5739
Yasin Hussein <bulaale116@gmail.com>
2024-01-16 15:15:23 -0700

    spinner1 and spinner2

---
+++ Asynchronous/hello_timeout.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+//Have our file print out these three words 4311o th3r3 w0r1d three seconds after the file is run.
+setTimeout(()=>{
+  console.log('4311o');
+},1000);
+setTimeout(()=>{
+  console.log('th3r3');
+},2000);
+setTimeout(() => {
+  console.log('W0r1d');
+}, 3000);
--- Asynchronous/spinner/spinner1.js
+++ Asynchronous/spinner/spinner1.js
@@ -23,3 +23,16 @@ setTimeout(() => {
 setTimeout(() => {
   process.stdout.write('\r \\/  ');
 }, 1100);
+
+
+
+/* eslint-disable linebreak-style */
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+let word = 'CODING';
+let backSlashes = ['|','','-','\\','/','\\'];
+for (let i = 0; i < word.length; i++) {
+  const timeDelay = 100 * (i + 1);
+  setTimeout(() => {
+    process.stdout.write(`\r${backSlashes[i]}  ${word[i]} `);
+  }, timeDelay);
+}
